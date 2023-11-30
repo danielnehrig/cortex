5987a54ca0eaf69f4e71e12eac87625729a305fc
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2023-11-30 05:17:54 +0100

    chore(test): fix test
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- cortex/src/lib.rs
+++ cortex/src/lib.rs
@@ -32,16 +32,16 @@
 //!         .add_statement(Statement::Table(&users, DbAction::Create))
 //!         .add_statement(Statement::Table(&orders, DbAction::Create))
 //!         .add_statement(Statement::Table(&users, DbAction::Drop));
-//!     let client_conf = ConnectionConfig::<Postgres>::default();
-//!     let cortex_conf = CortexPostgresConfig {
+//!     let _client_conf = ConnectionConfig::<Postgres>::default();
+//!     let _cortex_conf = CortexPostgresConfig {
 //!        plugins: vec![PostgresPlugins::Postgis, PostgresPlugins::Timescale],
 //!        supported_db_versions: (
 //!            semver::Version::new(15, 0, 0),
 //!            semver::Version::new(16, 0, 0),
 //!        ),
 //!     };
-//!     let connection = Postgres::new(client_conf).expect("to connect to db");
-//!     let producer = CortexPostgres::new(connection, cortex_conf).add_step(data);
+//!     // let connection = Postgres::new(_client_conf).expect("to connect to db");
+//!     // let producer = CortexPostgres::new(connection, _cortex_conf).add_step(data);
 //! ```
 //! to see more examples take a look into the examples folder
 //!
